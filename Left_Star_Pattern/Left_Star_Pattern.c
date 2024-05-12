0a06d108c6e37d55d205dd328eb9ffc4a16e8daa
Nancy Sharma <nancysharma2000a@gmail.com>
2024-05-12 20:25:00 +0530

    Create Left_Star_Pattern.c

---
+++ Left_Star_Pattern/Left_Star_Pattern.c
@@ -0,0 +1,18 @@
+ #include <stdio.h> 
+
+int main() 
+{ 
+	int rows = 5; 
+	for (int i = 0; i < rows; i++) { 
+
+		for (int j = 0; j < 2 * (rows - i) - 1; j++) { 
+			printf(" "); 
+		} 
+
+		for (int k = 0; k <= i; k++) { 
+			printf("* "); 
+		} 
+		printf("\n"); 
+	} 
+	return 0; 
+}
